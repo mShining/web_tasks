737b9c18d6990ee1dd470ae451d4e428c49f0355
mShining <miss.longlongago@qq.com>
2015-07-15 23:45:07 +0800

    修改：优化用户输入越界的处理

--- B0/fib.rb
+++ B0/fib.rb
@@ -2,43 +2,57 @@
 #150714 by mShining
 #new line every 10
 
+@flag=0
 
-puts "Please input an integer(1-100): "
 
-#get N
-@n = gets.to_i
-@a = 0
-@b = 1
-@t = 0
+while @flag===0
+  puts "Please input an integer(1-100): "
 
-puts
-print "The Fibonacci sequence-of-number(1-"
-print @n
-puts "):"
+  #get N
+  @n = gets.to_i
 
-#Xun-huan-shu-chu Fibonacci sequence-of-number
-@i = 1
-until ( (@i<=>@n) === 1 )
+  if (1<=@n and @n<=100)
+    @flag=1
 
-  #print "i'm here1"
+    @a = 0
+    @b = 1
+    @t = 0
 
-  if (@i === @n)
-    print @a
-    print "."
-    @i = @i + 1
-    next
+    puts
+    print "The Fibonacci sequence-of-number(1-"
+    print @n
+    puts "):"
+
+    #Xun-huan-shu-chu Fibonacci sequence-of-number
+    @i = 1
+    until ( (@i<=>@n) === 1 )
+
+      #print "i'm here1"
+
+      if (@i === @n)
+        print @a
+        print "."
+        @i = @i + 1
+        next
+      end
+
+      print @a
+      print ","
+      @t = @a;
+      @a = @b;
+      @b = @b + @t;
+
+      #new line every 10
+      if @i%10===0
+        print "\n"
+      end
+
+      @i = @i + 1
+    end
+
+
+  else
+    puts "The input is out of range , please input again!"
   end
 
-  print @a
-  print ","
-  @t = @a;
-  @a = @b;
-  @b = @b + @t;
-
-  #new line every 10
-  if @i%10===0
-    print "\n"
-  end
-
-  @i = @i + 1
 end
